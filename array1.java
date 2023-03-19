e187fa058cce983de989ffa8cfbc6efe31ae9fa0
Kirtijare125 <jarekirti1997@gmail.com>
2023-03-19 15:27:40 +0530

    "Added new file array1.java"

---
+++ array1.java
@@ -0,0 +1,34 @@
+package Assignment;
+
+public class array1 {
+	public static void main(String[] args) {
+		int [] arr= {5,3,1,2};
+		int n=arr.length + 1;
+		int sum =(n *(n+1))/2;
+		for(int i = 0; i < arr.length; i++)
+		{
+			sum=sum-arr[i];
+			
+		}
+		System.out.println("Missing number is " + sum);
+	}
+
+}
+
+
+package Assignment;
+
+public class array1 {
+	public static void main(String[] args) {
+		int [] arr= {5,3,1,2};
+		int n=arr.length + 1;
+		int sum =(n *(n+1))/2;
+		for(int i = 0; i < arr.length; i++)
+		{
+			sum=sum-arr[i];
+			
+		}
+		System.out.println("Missing number is " + sum);
+	}
+
+}
